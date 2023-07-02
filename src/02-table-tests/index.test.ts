3b55b63edb70b09c5a5e7045b733d4df02a11766
Siaw4ik <peretsviach@gmail.com>
2023-07-02 23:40:14 +0200

    feat: add 1 and 2 task of basic testing

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,29 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import {  simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+    { input: { a: 1, b: 2, action: Action.Add }, expected: 3 },
+    { input: { a: 1, b: 2, action: Action.Add }, expected: 3 },
+    { input: { a: 2, b: 2, action: Action.Add }, expected: 4 },
+    { input: { a: 3, b: 2, action: Action.Subtract }, expected: 1 },
+    { input: { a: 8, b: 2, action: Action.Subtract }, expected: 6 },
+    { input: { a: 4, b: 2, action: Action.Divide }, expected: 2 },
+    { input: { a: 10, b: 2, action: Action.Divide }, expected: 5 },
+    { input: { a: 3, b: 2, action: Action.Multiply }, expected: 6 },
+    { input: { a: 11, b: 1, action: Action.Multiply }, expected: 11 },
+    { input: { a: 3, b: 2, action: Action.Exponentiate }, expected: 9 },
+    { input: { a: 5, b: 2, action: Action.Exponentiate }, expected: 25 },
+    { input: { a: '3', b: 2, action: Action.Add }, expected: null },
+    { input: { a: 'five', b: 2, action: Action.Add }, expected: null },
+    { input: { a: 3, b: 2, action: 'plus' }, expected: null },
+    { input: { a: 3, b: 2, action: 'something' }, expected: null },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
-  });
-  // Consider to use Jest table tests API to test all cases above
+  test.each(testCases)(
+    'testing function using the table-driven testing approach, utilizing the appropriate Jest API',
+    ({ input, expected }) => {
+      expect(simpleCalculator(input)).toEqual(expected);
+    }
+  );
 });
